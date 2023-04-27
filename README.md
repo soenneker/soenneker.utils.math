bf1ec992161518e02662690f2097bc5b72cd2e19
Jake Soenneker <jake@soenneker.com>
2023-04-27 11:46:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Utils.Math.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Utils.Math/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.utils.math/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.utils.math/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Utils.Math.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Utils.Math/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Utils.Math
+### Some useful math related utility methods
+
+## Installation
+
+```
+Install-Package Soenneker.Utils.Math
+```
---
+++ src/MathUtil.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Soenneker.Extensions.Decimal;
+
+namespace Soenneker.Utils.Math;
+
+/// <summary>
+/// Some useful math related utility methods
+/// </summary>
+public static class MathUtil
+{
+    [Pure]
+    public static decimal GetWeightedMean(decimal valueA, decimal valueB, decimal weightA, decimal weightB, int? roundDigits = null)
+    {
+        decimal sumOfWeights = valueA * weightA + valueB * weightB;
+        decimal weightedAverage = sumOfWeights / (weightA + weightB);
+
+        if (roundDigits != null)
+            return weightedAverage.ToRounded(roundDigits.Value);
+
+        return weightedAverage;
+    }
+
+    [Pure]
+    public static decimal GetWeightedMean(List<Tuple<decimal, decimal>> valueWeights, int? roundDigits = null)
+    {
+        decimal sumOfWeightMultiplication = valueWeights.Sum(tuple => tuple.Item1 * tuple.Item2);
+
+        decimal sumOfWeights = valueWeights.Sum(tuple => tuple.Item2);
+
+        decimal weightedAverage = sumOfWeightMultiplication / sumOfWeights;
+
+        if (roundDigits != null)
+            return weightedAverage.ToRounded(roundDigits.Value);
+
+        return weightedAverage;
+    }
+
+    [Pure]
+    public static decimal GetMean(params decimal[] values)
+    {
+        if (values.Length == 0)
+            return 0;
+
+        decimal sum = values.Sum();
+
+        decimal result = sum / values.Length;
+        return result;
+    }
+
+    /// <summary>
+    /// (final / initial) / final.
+    /// If final = 0, sets final to .000001
+    /// </summary>
+    [Pure]
+    public static decimal GetRelativeChange(decimal initial, decimal final)
+    {
+        if (final == 0)
+            final = 0.00001M;
+
+        decimal result = (final - initial) / final;
+
+        return result;
+    }
+}
